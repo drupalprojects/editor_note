47c8667dd4f149b2f57578893cb94dcdf1d81868
Rostislav Sergeenkov <rostislav.sergeenkov@gmail.com>
2014-07-02 13:51:15 +0300

    js operations

---
+++ js/notes_field.js
@@ -0,0 +1,37 @@
+(function ($) {
+  Drupal.behaviors.notesField = {
+    attach: function(context) {
+      $('a.edit-note').click(function() {
+        // @todo drupal_html_id better than classes, because node can contain multiple note fields.
+        var delta = $(this).attr('data-delta');
+        var noteTrSelector = 'tr.note-' + delta;
+        var noteHiddenName = 'field_notes[und][' + delta + '][note]';
+        var noteValue = $(noteTrSelector).find('input[name="' + noteHiddenName + '"]').val();
+        $('textarea.add-note').val(noteValue);
+        $('input[name="field_notes_save_note"]').val(Drupal.t('Update Note'));
+        return false;
+      })
+
+      $('input.field_notes_save_note').click(function() {
+        // @todo validate on empty value first.
+        // @todo then check if it is add or it is edit mode.
+        $(this).val(Drupal.t('Add Note'));
+      })
+
+      $('a.remove-note').click(function() {
+        var delta = $(this).attr('data-delta');
+        var noteTrSelector = 'tr.note-' + delta;
+        var noteHiddenName = 'field_notes[und][' + delta + '][note]';
+        $('input[name="' + noteHiddenName + '"]').val('');
+        $(noteTrSelector).hide();
+          // @todo rebuild after that odd|even classes
+//        var tr = $(noteTrSelector).parent().find('tr');
+//        tr.each(function() {
+//          this.removeClass('odd', 'even');
+//          this.toggleClass();
+//        })
+        return false;
+      })
+    }
+  }
+})(jQuery);
