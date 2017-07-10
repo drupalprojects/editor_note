e2fc0e288924452dc36f69aaaadbaa30e460f417
Rostislav_Sergeenkov <rostislav.sergeenkov@gmail.com>
2017-07-10 16:01:17 +0300

    editor_note.api.php file was added to describe hook_editor_note_format_notes_alter()

---
+++ editor_note.api.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Editor Notes module.
+ */
+
+/**
+ * Hook is to allow other modules to alter the formatted notes
+ * before they are rendered.
+ *
+ * For example, if I wanted to display the note's author picture
+ * I would use custom hook_editor_note_format_notes_alter() to add this.
+ *
+ * @return
+ *   Renderable array of Editor notes.
+ */
+function hook_editor_note_format_notes_alter($formatted_notes) {
+
+}
